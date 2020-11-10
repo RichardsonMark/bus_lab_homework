af70be22ab2e7dfa1287bea55f771d591957e0d6
Mark Richardson <mark_richardson@sky.com>
2020-11-10 16:13:16 +0000

    Created bus class and checking passes first integration test

---
+++ bus_lab_homework/src/bus.py
@@ -0,0 +1,24 @@
+# TASK: Create a Bus class.
+#The Bus should have a route number (e.g. 22) and a destination (e.g. "Ocean Terminal").
+#The Bus should have a drive method that returns a string (e.g. "Brum brum").
+
+class Bus:
+    def __init__(self, route, destination):
+        self.route_number = route
+        self.destination = destination
+
+
+# write a method to get the test count passing
+    #def bus_drive(self):
+     #   return len(self.pets)
+
+# add a pet 
+    #def add_pet(self, new_pet):
+     #   self.pets.append(new_pet)
+
+# get total value of pets
+    #def get_total_value_of_pets(self):
+     #   total = 0
+     #   for pet in self.pets:
+     #       total += pet.price # or total = total.pet.price
+     #   return total
