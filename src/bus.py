85fb4cd291e92f4ab3895cf66803b3410b4c910f
Mark Richardson <mark_richardson@sky.com>
2020-11-10 17:01:55 +0000

    Created Drive method to return the requested string

--- src/bus.py
+++ src/bus.py
@@ -9,16 +9,6 @@ class Bus:
 
 
 # write a method to get the test count passing
-    #def bus_drive(self):
-     #   return len(self.pets)
+    def drive(self):
+        return "Brum brum"
 
-# add a pet 
-    #def add_pet(self, new_pet):
-     #   self.pets.append(new_pet)
-
-# get total value of pets
-    #def get_total_value_of_pets(self):
-     #   total = 0
-     #   for pet in self.pets:
-     #       total += pet.price # or total = total.pet.price
-     #   return total
